fcfd5681704e54763da5fe2164269c9dc1a8a82e
christian williams <christian.williamsj@gmail.com>
2019-09-16 22:05:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song 
   
-  attr_accessor :name, :artist, :genre
-  @@count = 0 
-  @@artists =[]
-  @@genre = []
+    attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
